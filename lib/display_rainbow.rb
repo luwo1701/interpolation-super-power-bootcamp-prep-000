6774fb651c9100a4936efc11b869c72159aeffd3
Luke Worley <luke.worley@colorado.edu>
2018-05-20 16:26:19 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,7 @@
 # Write your #display_rainbow method here
-letters=["R","O","Y","G","B","I","V"]
 def display_rainbow(colors)
+  letters=["R","O","Y","G","B","I","V"]
+
   for i in 0..colors.length-1
     puts "#{letters[i]}: #{colors},"
   end
